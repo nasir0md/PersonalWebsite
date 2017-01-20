f3aafc49bef89ba2955969a07be55f5b9bd6fa78
Andrew Szot <me@andrewszot.com>
2017-01-20 01:15:52 -0800

    Adding dist

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,1 @@
-ssh -i PersonalWebsiteKey.pem ec2-user@35.160.192.213
-cd andrew-website
-git fetch origin master
-git reset --hard origin/master
-mv dist/* /var/www/html
-exit
+ssh -i PersonalWebsiteKey.pem ec2-user@35.160.192.213 "cd andrew-website && git fetch origin master && git reset --hard origin/master && mv dist/* /var/www/html/ && exit"
